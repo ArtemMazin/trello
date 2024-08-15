c9d1de1dc613b43e4c52dcf49daedcf40ec2126e
ArtemMazin <minima1s@yandex.ru>
2024-08-15 13:17:36 +0300

    feat: создать структуру проекта. установить необходимые зависимости.

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+
+  dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder().setTitle('Trello API').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`Сервер стартовал на порту: ${PORT}`);
+  });
+}
+bootstrap();
